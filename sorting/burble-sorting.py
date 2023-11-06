1b5616d173e7ab79550d0511aad2a6dfc01ca618
Giancarlo Poem <giancarlo.poemape@hotmail.com>
2023-11-06 14:54:23 -0500

    adding burble sorting algorithm

---
+++ sorting/burble-sorting.py
@@ -0,0 +1,27 @@
+
+def burble_sort(numbers: list): # sort in ascend manner
+    changes = True
+    n_changes = 0
+    numbers_sort = numbers.copy()
+    while changes:
+        numbers_size = len(numbers)
+        n_changes = 0
+        for i in range(numbers_size):
+            print(f"i={i}")
+            if i + 1 < numbers_size: #if there are a next element	
+                if numbers_sort[ i ] > numbers_sort[ i + 1 ]:
+                    temp_val = numbers_sort[ i ] 
+                    numbers_sort[ i ]  = numbers_sort[ i + 1 ]
+                    numbers_sort[ i + 1 ] = temp_val
+                    
+                    n_changes += 1
+        if n_changes == 0:
+            changes = False
+                
+
+    return numbers_sort
+    
+
+arr = [6, 10, -1, 0, 20]
+
+print(burble_sort(arr))
